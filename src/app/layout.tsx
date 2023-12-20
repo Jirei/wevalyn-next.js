d20fdfb772b4f3b7950398d266a97b4671462348
Jirei <jirei.contact@gmail.com>
2023-12-20 22:19:53 +0100

    Added env.local.example and lazy loaded captcha script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({
         <Footer />
         <Script
           src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
+          strategy="lazyOnload"
         />
       </body>
     </html>
