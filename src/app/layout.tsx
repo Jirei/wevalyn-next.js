5f9cda896ca8408d5b0fe8b35ea1b3eda3e91da7
Jirei <jirei.contact@gmail.com>
2024-01-14 19:52:51 +0100

    Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,17 +57,33 @@ export default function RootLayout({
         <Script id="microsoft-clarity" strategy="lazyOnload">
           {getMicrosoftClarityScriptContent()}
         </Script>
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-0X8PL7R10X"
+        ></Script>
+        <Script id="google-analytics-sync">
+          {getGoogleAnalyticsSyncScriptContent()}
+        </Script>
       </body>
     </html>
   );
 }
 
 function getMicrosoftClarityScriptContent() {
-  return `
-          (function(c,l,a,r,i,t,y){
+  return `(function(c,l,a,r,i,t,y){
              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
            })(window, document, "clarity", "script", "kll2e7wfzo");
     `;
+}
+
+function getGoogleAnalyticsSyncScriptContent() {
+  return `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-0X8PL7R10X');
+  `;
 }
