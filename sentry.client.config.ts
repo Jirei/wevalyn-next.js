337586bf99bc21f8950c46f741098eef050397e4
Jirei <jirei.contact@gmail.com>
2024-02-01 03:35:00 +0100

    disabled sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,6 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  enabled: false,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NODE_ENV,
   // Replay may only be enabled for the client-side
